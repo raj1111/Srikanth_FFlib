add351ce3da8543f3516e0307c2c0a4204a1f735
speruricydcor <speruri@cydcor.com>
2019-04-02 14:29:53 -0700

    Committing the LWC components

--- cydcor-impl/main/lwc/core_customInfoContainer/core_customInfoContainer.js
+++ cydcor-impl/main/lwc/core_customInfoContainer/core_customInfoContainer.js
@@ -1,4 +1,6 @@
 import { LightningElement } from 'lwc';
+import createOpportunity from '@salesforce/apex/OpportunityController.createOpportunity';
+import {ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Core_customInfoContainer extends LightningElement {
 
@@ -7,5 +9,65 @@ export default class Core_customInfoContainer extends LightningElement {
     lastName = '';
     primaryPhoneNumber = '';
     secondaryPhoneNumber = '';
+    stage = '';
+    closeDate = '';
+
+    variant = 'success';
+    title = 'Status';
+    message = '';
+
+
+    captureFirstName(event){
+        this.firstName = event.target.value;
+    }
+    captureLasttName(event){
+        this.lastName = event.target.value;
+    }
+    captureMiddleName(event){
+        this.middleName = event.target.value;
+    }
+    capturePrimaryPhone(event){
+        this.primaryPhoneNumber = event.target.value;
+    }
+    captureSecondaryPhone(event){
+        this.secondaryPhoneNumber = event.target.value;
+    }
+    captureCloseDate(event){
+        this.closeDate = event.target.value;
+    }
+    captureStage(event){
+        this.stage = event.target.value;
+    }
+
+    saveOpportunity(){
+        console.log('Saving Opportunity');
+        const firstName = this.firstName;
+        // const lastName = this.lastName;
+        // const middleName = this.middleName;
+        // const primaryPhoneNumber = this.primaryPhoneNumber;
+        // const secondaryPhoneNumber = this.secondaryPhoneNumber;
+        const closeDate = this.closeDate;
+        const stage = this.stage;
+
+        createOpportunity({firstName, stage, closeDate})
+        .then(result => {
+            this.message = result;
+            this.showNotification();
+        })
+        .catch(error => {
+            this.message = 'Failed : ' + error;
+            this.variant = 'error';
+            this.showNotification();
+        });
+    }
+
+    showNotification(){
+        const evt = new ShowToastEvent({
+            title: this.title,
+            message: this.message,
+            variant: this.variant,
+        });
+        this.dispatchEvent(evt);
+    }
 
 }
